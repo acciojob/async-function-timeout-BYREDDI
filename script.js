17fbe15ec0dbf93d0b29bbea547d4d1a06b84b75
BYREDDI RAMANA BABU <127107319+BYREDDI@users.noreply.github.com>
2023-11-22 10:11:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 //your JS code here. If required.
+// Function to delay and display message
+async function displayMessage() {
+    const textInput = document.getElementById('text').value;
+    const delayInput = document.getElementById('delay').value;
+
+    if (!textInput || !delayInput) {
+        alert('Please enter both text and delay!');
+        return;
+    }
+
+    const outputDiv = document.getElementById('output');
+
+    try {
+        const delayInSeconds = parseInt(delayInput);
+        const message = await delayAndResolve(textInput, delayInSeconds);
+        outputDiv.innerText = message;
+    } catch (error) {
+        console.error(error);
+        outputDiv.innerText = 'Error occurred.';
+    }
+}
+
+// Async function to wait for a specified time before resolving with a message
+function delayAndResolve(message, delayInSeconds) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(message);
+        }, delayInSeconds * 1000);
+    });
+}
+
+// Event listener for button click
+const displayButton = document.getElementById('btn');
+displayButton.addEventListener('click', displayMessage);
